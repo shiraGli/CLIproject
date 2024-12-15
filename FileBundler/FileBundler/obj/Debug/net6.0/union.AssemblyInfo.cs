2454c4897c3449dd2be8911f5a77ddbbeb527871
shiraGli <shira179599@gmail.com>
2024-12-15 17:07:39 +0200

    my commit message

--- FileBundler/FileBundler/obj/Debug/net6.0/union.AssemblyInfo.cs
+++ FileBundler/FileBundler/obj/Debug/net6.0/union.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("union")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61735e22a7946596d4dbd3b86e5adf2b7d24e251")]
 [assembly: System.Reflection.AssemblyProductAttribute("union")]
 [assembly: System.Reflection.AssemblyTitleAttribute("union")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
